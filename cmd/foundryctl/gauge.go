8bfeda6a961d044183b9940f948d20f9ed5268a3
Miguel Valdes <mvaldes@signoz.io>
2025-12-26 13:03:09 -0600

    fix(foundryctl) gauge debug uses the command line argument now

--- cmd/foundryctl/gauge.go
+++ cmd/foundryctl/gauge.go
@@ -9,51 +9,50 @@ import (
 	"cuelang.org/go/cue"
 	"cuelang.org/go/cue/cuecontext"
 	"cuelang.org/go/cue/errors"
+	"cuelang.org/go/cue/load"
 	"cuelang.org/go/encoding/json"
 	"cuelang.org/go/encoding/yaml"
 	"github.com/SigNoz/foundry/internal/instrumentation"
 	"github.com/spf13/cobra"
-	"cuelang.org/go/cue/load"
 )
 
 var (
-	errorFilenotFound   = errors.New("File not found")
-	logger              = instrumentation.NewLogger(cfg.Debug).With(slog.String("cmd.name", "gauge"))
+	errorFilenotFound = errors.New("File not found")
 )
 
-var(
-	module = "github.com/signoz/foundry"
+const (
+	module     = "github.com/signoz/foundry"
 	schemaPath = "./internal/schema/casting.cue"
 )
 
-func loadSchema(ctx *cue.Context)(cue.Value, error){
+func loadSchema(ctx *cue.Context, logger *slog.Logger) (cue.Value, error) {
 	cfg := &load.Config{
-		Dir: ".",
+		Dir:    ".",
 		Module: module,
 	}
+	logger.Debug("Loading and compiling schema", slog.String("schema.path", schemaPath))
 
-	insts := load.Instances([]string{schemaPath}, cfg)
-	if len(insts) == 0{
-		return cue.Value{}, insts[0].Err
+	instance := load.Instances([]string{schemaPath}, cfg)
+	if len(instance) == 0 {
+		return cue.Value{}, instance[0].Err
 	}
 
-	return ctx.BuildInstance(insts[0]), nil
+	return ctx.BuildInstance(instance[0]), nil
 }
-
 
 func registerGaugeCmd(rootCmd *cobra.Command) {
 	gaugeCmd := &cobra.Command{
 		Use:   "gauge",
 		Short: "Gauge whether required tools are available.",
-		RunE: func(cmd *cobra.Command, args []string) error {
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			logger := instrumentation.NewLogger(cfg.Debug).With(slog.String("cmd.name", "gauge"))
 			ctx := cmd.Context()
-			filePath := cfg.File
-			err := validateConfig(filePath)
+			logger.DebugContext(ctx, "Starting Gauge command, using:", slog.String("cfg.file", cfg.File))
+			err := validateConfig(cfg.File, logger)
 			if err != nil {
 				logger.ErrorContext(ctx, "failed to validate config", slog.String("cfg.file", cfg.File), slog.String("error", err.Error()))
 			}
 
-			logger.DebugContext(ctx, "starting command", slog.String("cfg.file", cfg.File))
 			return nil
 		},
 	}
@@ -93,16 +92,16 @@ func compileDataFile(ctx *cue.Context, filename string, data []byte) (cue.Value,
 	return expr, err
 }
 
-
-func validateConfig(filename string) error {
+func validateConfig(filename string, logger *slog.Logger) error {
 	configFile, err := os.ReadFile(filename)
 	if err != nil {
 		return errorFilenotFound
 	}
+	logger.Debug("Read configuration file", slog.String("file.path", filename))
 
 	ctx := cuecontext.New()
 
-	schema, err := loadSchema(ctx)
+	schema, err := loadSchema(ctx, logger)
 	if err != nil {
 		return fmt.Errorf("schema compilation error:\n%s", errors.Details(err, nil))
 	}
