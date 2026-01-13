ddea1b058c2dc4465b6e230cbdb83e8c7aae9e20
Nagesh <nageshbansal59@gmail.com>
2026-01-13 15:18:26 +0530

    fix: refactor service templates

--- internal/casting/linuxcasting/embed.go
+++ internal/casting/linuxcasting/embed.go
@@ -12,7 +12,7 @@ var templates embed.FS
 var (
 	signozServiceTemplate *types.Template = types.MustNewTemplateFromFS(templates, "templates/signoz.service.gotmpl", types.FormatINI)
 	ingesterServiceTemplate *types.Template = types.MustNewTemplateFromFS(templates, "templates/ingester.service.gotmpl", types.FormatINI)
-	telemetryStoreServiceTemplate *types.Template = types.MustNewTemplateFromFS(templates, "templates/telemetrystore.clickhouse.v2556.service.gotmpl", types.FormatINI)
-	telemetryKeeperServiceTemplate *types.Template = types.MustNewTemplateFromFS(templates, "templates/telemetrykeeper.clickhousekeeper.v2556.service.gotmpl", types.FormatINI)
-	metaStoreServiceTemplate *types.Template = types.MustNewTemplateFromFS(templates, "templates/metastore.postgres.service.gotmpl", types.FormatINI)
+	telemetryStoreServiceTemplate *types.Template = types.MustNewTemplateFromFS(templates, "templates/clickhouse.telemetrystore.v2556.service.gotmpl", types.FormatINI)
+	telemetryKeeperServiceTemplate *types.Template = types.MustNewTemplateFromFS(templates, "templates/clickhousekeeper.telemetrykeeper.v2556.service.gotmpl", types.FormatINI)
+	metaStoreServiceTemplate *types.Template = types.MustNewTemplateFromFS(templates, "templates/postgres.metastore.service.gotmpl", types.FormatINI)
 )
--- internal/casting/linuxcasting/embed_test.go
+++ internal/casting/linuxcasting/embed_test.go
@@ -4,17 +4,24 @@ import (
 	"bytes"
 	"testing"
 
+	"github.com/signoz/foundry/internal/types"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestNotEmptyAndValid(t *testing.T) {
-	assert.NotEmpty(t, telemetryStoreServiceTemplate)
-	assert.NotEmpty(t, telemetryKeeperServiceTemplate)
-	assert.NotEmpty(t, metaStoreServiceTemplate)
-	assert.NotEmpty(t, signozServiceTemplate)
-	assert.NotEmpty(t, ingesterServiceTemplate)
-	buf := bytes.NewBuffer(nil)
-	err := signozServiceTemplate.Execute(buf, nil)
-	assert.NoError(t, err)
-	assert.NotEmpty(t, buf.String())
+	serviceTemplates := map[string]*types.Template{
+		"telemetryStoreServiceTemplate":  telemetryStoreServiceTemplate,
+		"telemetryKeeperServiceTemplate": telemetryKeeperServiceTemplate,
+		"metaStoreServiceTemplate":       metaStoreServiceTemplate,
+		"signozServiceTemplate":          signozServiceTemplate,
+		"ingesterServiceTemplate":        ingesterServiceTemplate,
+	}
+
+	for name, st := range serviceTemplates {
+		assert.NotEmpty(t, st, "%s should not be empty", name)
+		buf := bytes.NewBuffer(nil)
+		err := st.Execute(buf, nil)
+		assert.NoError(t, err, "error executing %s", name)
+		assert.NotEmpty(t, buf.String(), "%s output should not be empty", name)
+	}
 }
