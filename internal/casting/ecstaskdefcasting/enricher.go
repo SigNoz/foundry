1a9bba36dd788b92b9bd65ea6f43e67f6b22da58
Nagesh Bansal <nageshbansal59@gmail.com>
2026-03-19 01:00:08 +0530

    update conventions

--- internal/casting/ecstaskdefcasting/enricher.go
+++ internal/casting/ecstaskdefcasting/enricher.go
@@ -2,6 +2,7 @@ package ecstaskdefcasting
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/signoz/foundry/api/v1alpha1"
 	"github.com/signoz/foundry/internal/molding"
@@ -19,14 +20,14 @@ func newEcsMoldingEnricher() *ecsMoldingEnricher {
 func (enricher *ecsMoldingEnricher) EnrichStatus(ctx context.Context, kind v1alpha1.MoldingKind, config *v1alpha1.Casting) error {
 	switch kind {
 	case v1alpha1.MoldingKindTelemetryStore:
-		config.Spec.TelemetryStore.Status.Addresses.TCP = []string{types.FormatAddress("tcp", "telemetrystore", 9000)}
+		config.Spec.TelemetryStore.Status.Addresses.TCP = []string{types.FormatAddress("tcp", fmt.Sprintf("telemetrystore-%s", config.Spec.TelemetryStore.Kind.String()), 9000)}
 
 	case v1alpha1.MoldingKindTelemetryKeeper:
-		config.Spec.TelemetryKeeper.Status.Addresses.Client = []string{types.FormatAddress("tcp", "telemetrykeeper", 9181)}
-		config.Spec.TelemetryKeeper.Status.Addresses.Raft = []string{types.FormatAddress("tcp", "telemetrykeeper", 9234)}
+		config.Spec.TelemetryKeeper.Status.Addresses.Client = []string{types.FormatAddress("tcp", fmt.Sprintf("telemetrykeeper-%s", config.Spec.TelemetryKeeper.Kind.String()), 9181)}
+		config.Spec.TelemetryKeeper.Status.Addresses.Raft = []string{types.FormatAddress("tcp", fmt.Sprintf("telemetrykeeper-%s", config.Spec.TelemetryKeeper.Kind.String()), 9234)}
 
 	case v1alpha1.MoldingKindMetaStore:
-		config.Spec.MetaStore.Status.Addresses.DSN = []string{types.FormatAddress("tcp", "metastore", 5432)}
+		config.Spec.MetaStore.Status.Addresses.DSN = []string{types.FormatAddress("tcp", fmt.Sprintf("metastore-%s", config.Spec.MetaStore.Kind.String()), 5432)}
 
 	case v1alpha1.MoldingKindSignoz:
 		config.Spec.Signoz.Status.Addresses.APIServer = []string{types.FormatAddress("tcp", "signoz", 8080)}
